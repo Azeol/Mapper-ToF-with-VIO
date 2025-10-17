00ece426cb2a38ec619cad75939bb1b51738f6d5
Azeol <neser0067@gmail.com>
2025-10-17 15:49:23 +0200

    Changing VHDL comments

--- FPGA_NIOS_Mapper/i2c_driver_e.vhd
+++ FPGA_NIOS_Mapper/i2c_driver_e.vhd
@@ -2,36 +2,32 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity i2c_driver_e is
-    Port (
-        -- Pins physiques I2C
-        SDA    : inout std_logic;
-        SCL    : inout std_logic;
+	Port (
+		-- I2C Physical pins
+		SDA    : inout std_logic;
+		SCL    : inout std_logic;
 
-        -- Horloge et reset (optionnel selon ton design)
-        clk    : in std_logic;
-        rst_n  : in std_logic;
+		-- RST and CLK
+		clk    : in std_logic;
+		rst_n  : in std_logic;
 
-        -- Signaux venant du module I2C
-        sda_oe : in std_logic;
-        scl_oe : in std_logic;
-        sda_in : out std_logic;
-        scl_in : out std_logic
-    );
+		-- Signal coming from Nios2
+		sda_oe : in std_logic;
+		scl_oe : in std_logic;
+		sda_in : out std_logic;
+		scl_in : out std_logic
+	);
 end i2c_driver_e;
 
 architecture i2c_driver_a of i2c_driver_e is
 begin
+	
+	--SDA
+   SDA <= '0' when sda_oe = '1' else 'Z';  -- open-drain
+   sda_in <= SDA;                          -- bus reading
 
-    -- ==============================
-    -- Gestion bidirectionnelle SDA
-    -- ==============================
-    SDA <= '0' when sda_oe = '1' else 'Z';  -- open-drain
-    sda_in <= SDA;                          -- lecture du bus
-
-    -- ==============================
-    -- Gestion bidirectionnelle SCL
-    -- ==============================
-    SCL <= '0' when scl_oe = '1' else 'Z';
-    scl_in <= SCL;
+   -- SCL
+   SCL <= '0' when scl_oe = '1' else 'Z';
+   scl_in <= SCL;
 
 end i2c_driver_a;
