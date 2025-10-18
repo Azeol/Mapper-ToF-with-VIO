121d209a33c3362601b7717f08e50e70a6867966
Azeol <neser0067@gmail.com>
2025-10-18 16:45:08 +0200

    Pushing before updating and regenerating BSP because of a change in bit size for lidar UART (didn't read the datasheet earl enough)

--- FPGA_NIOS_Mapper/Hardware_Qsys/synthesis/submodules/Hardware_Qsys_sysid_qsys_0.v
+++ FPGA_NIOS_Mapper/Hardware_Qsys/synthesis/submodules/Hardware_Qsys_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module Hardware_Qsys_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1760717066 : 305419896;
+  assign readdata = address ? 1760798600 : 305419896;
 
 endmodule
 
